46726aa69a146452bd321c1ae319af757521fb4d
Maria Jose Loperfido <majoloperfido@hotmail.com>
2023-05-31 08:05:57 -0300

    cambios miercoles 31/5 en casa

--- reactejercicio.js
+++ reactejercicio.js
@@ -1,9 +1,9 @@
-let dato = prompt(parseInt("ingrese el dato"));
+// let dato = prompt(parseInt("ingrese el dato"));
 
 
-if (dato >= 10) {
-    (dato + 2)
-    document.write(dato)
-}else{
-    document.write("es menor")
-}
+// if (dato >= 10) {
+//     (dato + 2)
+//     document.write(dato)
+// }else{
+//     document.write("es menor")
+// }
